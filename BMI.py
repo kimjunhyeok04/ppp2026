a2c03a36e6784bea20af4a29fb3cc8654fd089a4
kimjunhyeok04 <suzhsbsu@gmail.com>
2026-03-10 14:17:17 +0900

    Add files via upload

---
+++ BMI.py
@@ -0,0 +1,4 @@
+weight = 60
+height = 1.7
+BMI = weight / (height*height)
+print(BMI)
---
+++ easy.py
@@ -0,0 +1,13 @@
+r = 4
+suclespace = (r*r)*3.14
+print(f"원의 면적은 {suclespace}입니다.")
+
+b = 5
+a = 3
+h = 4
+s = (a + b)*4 / 2
+print(f"사다리꼴의 넓이는 {s}입니다")
+
+g = 2000
+price = g - (g * 15 / 100)
+print(f"할인된 가격은 {price}입니다")
