83fe3532ead850d81ed789f5992abcd188a78784
kimjunhyeok04 <suzhsbsu@gmail.com>
2026-03-28 20:11:40 +0900

    Add files via upload

---
+++ lec03/distance.py
@@ -0,0 +1,11 @@
+X1 = input("X1 좌표를 입력해주세요")
+X1 = int(X1)
+Y1 = input("Y1 좌표를 입력해주세요")
+Y1 = int(Y1)
+X2 = input("X2 좌표를 입력해주세요")
+X2 = int(X2)
+Y2 = input("Y2 좌표를 입력해주세요")
+Y2 = int(Y2)
+import math
+distance =math.sqrt((math.pow(abs(X1 -X2),2)) + (math.pow(abs(Y1 - Y2), 2)))
+print(f"두 지점 사이의 거리는 {distance}입니다")
---
+++ lec03/input c-f.py
@@ -0,0 +1,4 @@
+temp_c = float(input("온도를 입력하시오."))
+temp_c = int(temp_c)
+temp_f = (temp_c * 1.8) + 32
+print(f"{temp_c}C => {temp_f}F")
